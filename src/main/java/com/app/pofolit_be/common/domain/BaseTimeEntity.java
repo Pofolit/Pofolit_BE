5325fb49597429d0915915937ce3ce8bebe5c7d6
jj <lim104321@naver.com>
2025-08-29 15:22:18 +0900

    feat: BaseTimeEntity 추가 - 공통 타임 스탬프 기능을 위해 추가

---
+++ src/main/java/com/app/pofolit_be/common/domain/BaseTimeEntity.java
@@ -0,0 +1,19 @@
+package com.app.pofolit_be.common.domain;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public class BaseTimeEntity {
+
+    @CreationTimestamp
+    private LocalDateTime createTime;
+
+    @UpdateTimestamp
+    private LocalDateTime updateTime;
+}
