6ee8864a1af42b2e7d2ecb05b08d8260cf0365f0
글지대 <131014787+ruukr8080@users.noreply.github.com>
2025-09-18 00:19:58 +0900

    Revert "Main"

--- src/main/java/com/app/pofolit_be/user/dto/SignDto.java
+++ src/main/java/com/app/pofolit_be/user/dto/SignDto.java
@@ -1,17 +1,5 @@
 package com.app.pofolit_be.user.dto;
 
-import com.app.pofolit_be.user.entity.Role;
-import com.app.pofolit_be.user.entity.User;
-
-/**
- * DB의 사용자 정보를 저장할 용도
- *
- * @param email
- * @param nickname
- * @param profileImageUrl
- * @param registrationId
- * @param providerId providerId의 역할은 여기서 끝.
- */
 public record SignDto(
         String email,
         String nickname,
@@ -20,14 +8,4 @@ public record SignDto(
         String providerId
 )
 {
-    public User toEntity() {
-        return User.builder()
-                .email(this.email)
-                .nickname(this.nickname)
-                .profileImageUrl(this.profileImageUrl)
-                .registrationId(registrationId)
-                .providerId(providerId)
-                .role(Role.GUEST)
-                .build();
-    }
 }
--- src/main/java/com/app/pofolit_be/user/repository/UserRepository.java
+++ src/main/java/com/app/pofolit_be/user/repository/UserRepository.java
@@ -1,17 +1,15 @@
 package com.app.pofolit_be.user.repository;
 
 import com.app.pofolit_be.user.entity.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
 
-    Optional<User> findUserByEmail(String email);
+   Optional<User> findByRegistrationIdAndProviderId(String registrationId, String providerId);
 
-    Optional<User> findByRegistrationIdAndProviderId(String registrationId, String providerId);
-
-    Optional<User> findByProviderId(String providerId);
-
-    Optional<User> findUserById(Long id);
+   Optional<User> findUserByEmail(String email);
 }
